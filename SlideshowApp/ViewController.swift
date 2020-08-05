a1199f94473acca16d812443b6f9f928c906d129
tomomi.koyama <smalllamp.t.k@gmail.com>
2020-08-05 16:10:45 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 小山燈実 on 2020/08/05.
+//  Copyright © 2020 tomomi.koyama. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
